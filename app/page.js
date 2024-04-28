a8036d58c93f21e7313c6890b125a5c8bd1d9f33
Jay Nguyen <bobguy0606@gmail.com>
2024-04-28 07:11:36 -0700

    prod

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import ProductsList from "../components/products/ProductsList";
 
 const getProducts = async (searchParams) => {
   try {
-    let currentPage = parseInt(searchParams.page || "1");
+    let currentPage =searchParams.page || "1";
 
     const response = await fetch(
       `${process.env.BASE_URL}/api/products?page=${currentPage}`,
