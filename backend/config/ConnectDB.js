6fde7b6da2aa0d8d5754daa5211ca9167349ebed
Jay Nguyen <bobguy0606@gmail.com>
2024-04-27 23:43:37 -0700

    debug

--- backend/config/ConnectDB.js
+++ backend/config/ConnectDB.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
 
 const dbConnect = async () => {
-  try {
+ 
     if (mongoose.connection.readyState >= 1) {
       console.log("already connectd to database");
     }
     mongoose.set("strictQuery", false);
     mongoose.connect(process.env.DB_URI);
-  } catch (error) {
-    console.log(error);
-  }
+  
 };
 
 export default dbConnect;
