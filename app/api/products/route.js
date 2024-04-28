be201e8f446e2a9755962e0ff04c22a850918576
Jay Nguyen <bobguy0606@gmail.com>
2024-04-28 06:00:49 -0700

    use client failed

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -11,10 +11,10 @@ export async function GET(req) {
     const currentPage = parseInt(searchParams.get("page")) || 1;
     const skipHowMany = 3 * (currentPage - 1);
     const allProducts = await Product.find().skip(skipHowMany).limit(3);
-    const totalItems = await Product.countDocuments();
-    const maxPages = Math.ceil(totalItems / 3);
+    // const totalItems = allProducts.length;
+    // const maxPages = Math.ceil(totalItems / 3);
 
-    return NextResponse.json({ products: allProducts, maxPages: maxPages });
+    return NextResponse.json({ products: allProducts, maxPages: 50 });
   } catch (error) {
     return NextResponse.json(error);
   }
