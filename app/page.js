68d2fa670b38121af43571f8a72e927dbc261650
Jay Nguyen <bobguy0606@gmail.com>
2024-04-29 06:47:55 -0700

    testing actions

--- app/page.js
+++ app/page.js
@@ -1,6 +1,23 @@
 import HeroSection from "@/components/Homepage/HeroSection";
 import ProductsList from "../components/products/ProductsList";
-import { getProducts } from "./action";
+// import { getProducts } from "./action";
+
+const getProducts = async (searchParams) => {
+  try {
+    const currentPage = Number(searchParams?.page) || 1;
+
+    const response = await fetch(
+      `${process.env.BASE_URL}/api/products?page=${currentPage}`,
+      {
+        method: "GET",
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
 
 export default async function Home({ searchParams }) {
   const { products, nextPageLink, prevPageLink } = await getProducts(
