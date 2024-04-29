aad66362178ecf6ffb46a4f093ae67887be26a5a
Jay Nguyen <bobguy0606@gmail.com>
2024-04-28 22:13:04 -0700

    link pathnames not being recognized

--- components/products/ProductsList.jsx
+++ components/products/ProductsList.jsx
@@ -1,10 +1,9 @@
-"use client";
 import React from "react";
 import ProductItem from "./ProductItem";
 import Filters from "../utilities/Filters";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+
 const ProductsList = ({ data, nextPageLink, prevPageLink }) => {
-  const router = useRouter();
   return (
     <section className="py-12">
       <div className="container max-w-screen-xl mx-auto px-4">
@@ -19,23 +18,19 @@ const ProductsList = ({ data, nextPageLink, prevPageLink }) => {
         </div>
         <div>
           <div className="flex justify-center gap-5">
-            <button
-              onClick={() => {
-                router.push(prevPageLink);
-              }}
+            <Link
+              href={`${prevPageLink}`}
               className="bg-blue-500 text-white rounded-md p-2"
             >
               prev. page
-            </button>
+            </Link>
 
-            <button
-              onClick={() => {
-                router.push(nextPageLink);
-              }}
+            <Link
+              href={`${nextPageLink}`}
               className="bg-blue-500 text-white rounded-md p-2"
             >
               next page
-            </button>
+            </Link>
           </div>
         </div>
       </div>
